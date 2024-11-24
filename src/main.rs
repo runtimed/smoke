421773c36ee863055538656bf2458a0a4320846f
Kyle Kelley <rgbkrk@gmail.com>
2024-11-23 21:04:29 -0800

    full cycle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
-use futures::{io::BufReader, AsyncBufReadExt as _, StreamExt as _};
+use futures::{
+    io::BufReader, AsyncBufReadExt as _, AsyncReadExt as _, SinkExt as _, StreamExt as _,
+};
 use gpui::{App, AppContext, AsyncAppContext};
-use http_client::{AsyncBody, HttpClient};
+use http_client::{AsyncBody, HttpClient, Request};
+use jupyter_protocol::ExecuteRequest;
+use jupyter_websocket_client::{KernelLaunchRequest, RemoteServer};
 use mybinder::parse_binder_build_response;
 
 use reqwest_client::ReqwestClient;
@@ -32,6 +36,8 @@ fn app_main(cx: &mut AppContext) {
         let reader = BufReader::new(response.into_body());
         let mut stream = reader.lines().boxed();
 
+        let mut remote_server: Option<RemoteServer> = None;
+
         while let Some(line_result) = stream.next().await {
             match line_result {
                 Ok(line) => {
@@ -42,6 +48,12 @@ fn app_main(cx: &mut AppContext) {
                         Ok(build_response) => match build_response.phase {
                             mybinder::Phase::Ready { url, token, .. } => {
                                 println!("Binder is ready! URL: {}, Token: {}", url, token);
+
+                                remote_server = Some(RemoteServer {
+                                    base_url: url,
+                                    token,
+                                });
+
                                 break;
                             }
                             mybinder::Phase::Failed { message } => {
@@ -63,19 +75,85 @@ fn app_main(cx: &mut AppContext) {
             }
         }
 
-        // for line in lines {
-        // let response = parse_binder_build_response(&line)?;
+        let remote_server =
+            remote_server.ok_or(anyhow::anyhow!("Binder did not start successfully"))?;
 
-        // Connect to a kernel on binder
+        let kernel_launch_request = KernelLaunchRequest {
+            name: "python".to_string(),
+            path: None,
+        };
 
-        // Execute code
+        let kernel_launch_request = serde_json::to_string(&kernel_launch_request)?;
 
-        // Verify code executed
-        //
-        //
-        cx.update(|cx| {
-            cx.quit();
+        let request = Request::builder()
+            .method("POST")
+            .uri(&remote_server.api_url("/kernels"))
+            .header("Authorization", format!("token {}", remote_server.token))
+            .body(AsyncBody::from(kernel_launch_request))?;
+
+        let response = http_client.send(request).await?;
+
+        if !response.status().is_success() {
+            let mut body = String::new();
+            response.into_body().read_to_string(&mut body).await?;
+            anyhow::bail!("Failed to launch kernel: {}", body);
+        }
+
+        let mut body = String::new();
+        response.into_body().read_to_string(&mut body).await?;
+
+        let response: jupyter_websocket_client::Kernel = serde_json::from_str(&body)?;
+
+        let kernel_id = response.id;
+
+        let (ws, _ws_response) = remote_server.connect_to_kernel(&kernel_id).await?;
+
+        let (mut w, mut r) = ws.split();
+
+        cx.spawn(|cx| async move {
+            while let Some(message) = r.next().await {
+                match message {
+                    Ok(message) => {
+                        // Normally would update a model here
+                        match message.content {
+                            jupyter_protocol::JupyterMessageContent::ExecuteResult(
+                                execute_result,
+                            ) => {
+                                println!("Got execute result: {:?}", execute_result);
+
+                                return cx.update(|cx| {
+                                    cx.quit();
+                                });
+                            }
+                            _ => {
+                                dbg!(&message.content);
+                            }
+                        }
+                    }
+                    Err(e) => eprintln!("Error reading message: {:?}", e),
+                }
+            }
+            anyhow::Ok(())
         })
+        .detach();
+
+        cx.background_executor().timer(Duration::from_secs(1)).await;
+
+        w.send(
+            ExecuteRequest {
+                code: "2 + 2".to_string(),
+                store_history: false,
+                allow_stdin: false,
+                stop_on_error: false,
+                silent: false,
+                user_expressions: None,
+            }
+            .into(),
+        )
+        .await?;
+
+        cx.background_executor().timer(Duration::from_secs(1)).await;
+        Ok(())
     })
     .detach_and_log_err(cx);
 }
